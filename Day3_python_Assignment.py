440319f094eb9974b0bb63e185a24853b9806332
Ashiya123 <68660943+Ashiya123@users.noreply.github.com>
2020-07-23 11:42:07 +0530

    Add files via upload

---
+++ Day3_python_Assignment.py
@@ -0,0 +1,23 @@
+##Question 1 : Find sum of n numbers with help of While loop
+num=int(input("Enter the Number"));
+print("You have entered the number is :-->",num);
+
+sum=0;
+print (num)
+i=0
+while i<num:
+    sum=sum+i
+    print(i)
+    print("Sum",sum);
+    i=i+1
+
+##Question 2 : Program to check if a number is prime or not
+
+Num1=int(input("Enter the number"))
+
+print("Entered number is :-->",Num1)
+if Num1%2==0:
+    
+    print("Entered number is prime")
+else:
+    print("Enterd number is not prime")
